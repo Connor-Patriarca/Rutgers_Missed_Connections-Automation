ee0b72066825cb02c554310abf9ce11a40df9e11
Connor Patriarca <93213245+Connor-Patriarca@users.noreply.github.com>
2022-10-17 23:44:45 -0400

    Big changes, closer to goal, don't have brain space to say more

--- PrevPostTime.py
+++
@@ -1,2 +0,0 @@
-lastposttime = '9/12/2022 23:28:11'
-placeholderposttime = '9/12/2022 23:28:11'
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import gspread
 from datetime import datetime
 
+# Prompt Script to start looking from when?
+dateprompt = input(str("\nWhat date/time should I start working from?\nFormat is 'mm/dd/yyyy hh:mm:ss' : "))
+
+# Create variables with that date
+lastposttime = datetime.strptime(dateprompt, '%m/%d/%Y %H:%M:%S')
+placeholderposttime = datetime.strptime(dateprompt, '%m/%d/%Y %H:%M:%S')
+
+
 # Pull entries from Google Sheet
 gc = gspread.service_account('test-project-364817-58d0da683a9b.json')
 sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1GAGsJhb3pH9WLwLs7Kj9F_h0x0lweRL30qURPBguGfw/edit?usp=sharing')
@@ -12,19 +20,16 @@ postrow = 2
 post = worksheet.get('A{}'.format(postrow)).first()
 posttime = worksheet.get('B{}'.format(postrow)).first()
 # Translating Date
-posttime_datetime_object = datetime.strptime(posttime, '%m/%d/%Y %H:%M:%S')
+currently_pulled_post_time = datetime.strptime(posttime, '%m/%d/%Y %H:%M:%S')
 
 # Script chooses to continue or not based on the time of the last generated post
-from PrevPostTime import lastposttime, placeholderposttime
-
-lastposttime = datetime.strptime(lastposttime, '%m/%d/%Y %H:%M:%S')
-placeholderposttime = datetime.strptime(placeholderposttime, '%m/%d/%Y %H:%M:%S')
-
+print(currently_pulled_post_time)
 print(lastposttime,placeholderposttime)
+print(type(lastposttime),type(placeholderposttime))
 
-while posttime_datetime_object > lastposttime:
+while currently_pulled_post_time > lastposttime:
     # Generate Image
-    import GenerateImage
+    import GenerateImage 
     posttime_datetime_object = placeholderposttime
     postrow += 1
     

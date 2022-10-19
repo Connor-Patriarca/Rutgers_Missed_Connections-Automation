dbd859ec1049b13240c6508cc9d5cd547df345e6
Connor Patriarca <93213245+Connor-Patriarca@users.noreply.github.com>
2022-10-18 21:15:18 -0400

    Main.py properly iterates through dates

--- main.py
+++ main.py
@@ -6,12 +6,10 @@ from GenerateImage import generateImage
 dateprompt = input(str("\nWhat date/time should I start working from?\nFormat is 'mm/dd/yyyy hh:mm:ss' : "))
 
 # Create variables with that date
-lastposttime = datetime.strptime(dateprompt, '%m/%d/%Y %H:%M:%S')
-placeholderposttime = datetime.strptime(dateprompt, '%m/%d/%Y %H:%M:%S')
-
+LastImageTime = datetime.strptime(dateprompt, '%m/%d/%Y %H:%M:%S')
 
 # Pull entries from Google Sheet
-gc = gspread.service_account('test-project-364817-58d0da683a9b.json')
+gc = gspread.service_account('rutgers-missed-connections-b4bf6b9aa508.json')
 sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1GAGsJhb3pH9WLwLs7Kj9F_h0x0lweRL30qURPBguGfw/edit?usp=sharing')
 worksheet = sh.sheet1
 
@@ -21,20 +19,20 @@ postrow = 2
 post = worksheet.get('A{}'.format(postrow)).first()
 posttime = worksheet.get('B{}'.format(postrow)).first()
 # Translating Date
-currently_pulled_post_time = datetime.strptime(posttime, '%m/%d/%Y %H:%M:%S')
+posttime_datetime_object = datetime.strptime(posttime, '%m/%d/%Y %H:%M:%S')
 
 # Script chooses to continue or not based on the time of the last generated post
-print(currently_pulled_post_time)
-print(lastposttime,placeholderposttime)
-print(type(lastposttime),type(placeholderposttime))
+print(posttime_datetime_object)
 
-while currently_pulled_post_time > lastposttime:
+while posttime_datetime_object >= LastImageTime:
     # Generate Image
     generateImage(post)
-    posttime_datetime_object = placeholderposttime
     postrow += 1
-    
-lastposttime = placeholderposttime
+    # Post and Time
+    post = worksheet.get('A{}'.format(postrow)).first()
+    posttime = worksheet.get('B{}'.format(postrow)).first()
+    # Translating Date
+    posttime_datetime_object = datetime.strptime(posttime, '%m/%d/%Y %H:%M:%S')
 
-# # Print Results
-# print(post,'\n',type(posttime_datetime_object),posttime_datetime_object)
+# For when script is put on timer    
+posttime_datetime_object = LastImageTime

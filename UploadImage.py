f2069e194bf81a56baa747cc782c7d1cb631d9b1
Connor Patriarca <93213245+Connor-Patriarca@users.noreply.github.com>
2022-11-09 12:51:22 -0500

    Drive Folder Tweak

--- UploadImage.py
+++ UploadImage.py
@@ -9,7 +9,7 @@ SCOPES = ['https://www.googleapis.com/auth/drive']
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
 def UploadImage(image_path,date):
-    folder_id = '1b1KE_71vPNoO02-JGsTH0uyEHEdVEW5N'
+    folder_id = '1F5WX2BonUA0ralcOGpJCJZjrL4DFytJZ'
     mime_type = 'image/jpeg'
 
     file_metadata = {

f9d07cf6085262689403e870993fc80a5d44baaf
Connor Patriarca <93213245+Connor-Patriarca@users.noreply.github.com>
2022-12-21 21:08:38 -0500

    fail

--- src/tests/UploadImageTest.py
+++ src/tests/UploadImageTest.py
@@ -5,6 +5,9 @@ class TestStringMethods(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
+    def test_upper_FAIL(self):
+        self.assertEqual('foo'.upper(), 'FOO00000')
+
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
         self.assertFalse('Foo'.isupper())
